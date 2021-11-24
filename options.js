fd7d63022ee1526149d980f35fa0b7ee010c8b39
BrandonZV <35254141+BrandonZV@users.noreply.github.com>
2021-11-23 18:59:38 -0600

    Add files via upload

---
+++ options.js
@@ -0,0 +1,95 @@
+function saveConfig(){
+	var temp = document.getElementById("college");
+	var collegeOption = temp.value;
+	chrome.storage.local.set({"collegeOption": collegeOption});
+	
+	var checklist = ["checkbox-select", 'checkbox-submitted', 'checkbox-location', 'checkbox-arrival',
+						'checkbox-fname', 'checkbox-lname', 'checkbox-studentid', 'checkbox-college-email',
+						'checkbox-alt-email', 'checkbox-paper-comp-test', 'checkbox-course-name-number',
+						'checkbox-section-number', 'checkbox-instructor', 'checkbox-test-number',
+						'checkbox-lecture-lab', 'checkbox-staff-queue-number', 'checkbox-staff-test-number',
+						'checkbox-staff-seat-number', 'checkbox-staff-scratch-paper', 'checkbox-staff-graph-paper',
+						'checkbox-staff-calculator', 'checkbox-staff-permitted-items', 'checkbox-staff-start-time',
+						'checkbox-staff-end-time', 'checkbox-staff-session-ended', 'checkbox-academic-dishonesty',
+						'checkbox-additional-comments'
+					];
+	
+	var checklist2 = {};
+	
+	for (i in checklist) {
+		var tester = document.getElementById(checklist[0]).checked;
+		checklist2[checklist[i]] = document.getElementById(checklist[i]).checked;
+		
+	}
+	// console.log(checklist2);
+	
+	chrome.storage.local.set({checklist2});
+
+	// var testl = ['a','b','c'];
+
+	// chrome.storage.local.get("checkbox-select", function(obj) {
+		// console.log(obj)
+	// });
+	
+}
+function constructor(){
+	// console.log("constructor");
+	
+	var checklist5 = ['checkbox-select', 'checkbox-submitted', 'checkbox-location', 'checkbox-arrival',
+						'checkbox-fname', 'checkbox-lname', 'checkbox-studentid', 'checkbox-college-email',
+						'checkbox-alt-email', 'checkbox-paper-comp-test', 'checkbox-course-name-number',
+						'checkbox-section-number', 'checkbox-instructor', 'checkbox-test-number',
+						'checkbox-lecture-lab', 'checkbox-staff-queue-number', 'checkbox-staff-test-number',
+						'checkbox-staff-seat-number', 'checkbox-staff-scratch-paper', 'checkbox-staff-graph-paper',
+						'checkbox-staff-calculator', 'checkbox-staff-permitted-items', 'checkbox-staff-start-time',
+						'checkbox-staff-end-time', 'checkbox-staff-session-ended', 'checkbox-academic-dishonesty',
+						'checkbox-additional-comments'
+					];
+	
+	// console.log(checklist5);
+	var checklist2 = '';
+	chrome.storage.local.get({checklist2}, function(obj) {
+		// console.log(obj);
+		// console.log(obj.checklist2);
+		var list = obj.checklist2;
+		// console.log(list);
+		for (var i in list) {
+			// console.log(i);
+			// console.log(list[i]);
+			document.getElementById(i).checked = list[i];
+		}
+			
+	});
+	
+	chrome.storage.local.get("collegeOption", function(obj) {
+		// console.log(obj["collegeOption"]);
+		document.getElementById("college").value = obj["collegeOption"];
+	});
+	
+
+	 // TODO make this spretty, I'm just not skilled enough
+		// for (let j = 0; j <checklist5.length; j++) {
+			// var t = checklist5[j];
+			// console.log('t is: ' + t);
+			// chrome.storage.local.get({[t]: true}, function(obj) {
+				// console.log(j);
+				// console.log(t);
+				// console.log(obj);
+				// document.getElementById(object.keys(obj)[0]).checked = obj[object.keys(obj)[0]];
+			// });
+		// }
+
+	//chrome.storage.local.get({checklist5}, function(obj) {
+	//	console.log('inside get');
+	//	console.log(obj);
+	//	for (var i in obj) {
+	//		console.log(i);
+	//		console.log(obj[i]);
+	//		console.log(checklist5[i]);
+	//		document.getElementById(checklist5[i]).checked = obj[i];
+	//	}
+	//});
+}
+
+document.getElementById('saveSettings').addEventListener('click', saveConfig);
+constructor();
---
+++ popup.js
@@ -0,0 +1,48 @@
+function injectTheScript_filters() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        // query the active tab, which will be only one tab
+        //and inject the script in it
+		chrome.tabs.executeScript(tabs[0].id, {file: "set_filters.js"});
+    });
+}
+
+function injectTheScript_columns() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        // query the active tab, which will be only one tab
+        //and inject the script in it
+		
+		chrome.tabs.executeScript(tabs[0].id, {file: "set_columns.js"});
+    });
+}
+
+// async function getCurrentTab() {
+	// let queryOptions = { active: true, currentWindow: true };
+	// let [tab] = await chrome.tabs.query(queryOptions);
+	// return tab;
+// }
+
+// function load(){
+	// window.addEventListener('load', function () {
+		// injectTheScript();
+	// });
+// }
+
+function linkLibWizard() {
+    chrome.tabs.create({
+      url: 'https://dcccd.libwizard.com/forms/reports/1f6ee9b57ba80b87122a49a7ac56273d'
+    });
+	//let tab = getCurrentTab();
+	
+	//chrome.tabs.executeScript(tab[0].id, {code: "load"});
+	
+}
+
+// var test = {a:1, b:'c'};
+
+// chrome.storage.local.set({test});
+
+document.getElementById('filters').addEventListener('click', injectTheScript_filters);
+
+document.getElementById('goLibWizard').addEventListener('click', linkLibWizard);
+
+document.getElementById('columns').addEventListener('click', injectTheScript_columns);
